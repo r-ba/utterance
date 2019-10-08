c9eba8b4268d2abb3b901e73072810dccf21781f
r-ba <rhysbalevicius@gmail.com>
2019-10-07 23:27:49 -0400

    Added default search results.

---
+++ client/src/defaultData.js
@@ -0,0 +1,45 @@
+const data = {
+  "items" :
+    {
+      "0" : {
+        "id" : "Eco2s3-0zsQ",
+        "title" : "Joe Rogan Experience #1361 - Cmdr. David Fravor &amp; Jeremy Corbell",
+        "thumbnail" : {
+          "url" : "https://i.ytimg.com/vi/Eco2s3-0zsQ/default.jpg",
+          "width" : 120,
+          "height" : 90
+        },
+        "matches" : [
+          { "start" : "4002.839", "dur" : "3.45", "text" : "also possible that that men and black" }
+        ]
+      },
+      "1" : {
+        "id" : "EnFAfg4gQDQ",
+        "title" : "Joe Rogan Experience #1358 - Sober October 3",
+        "thumbnail" : {
+          "url" : "https://i.ytimg.com/vi/EnFAfg4gQDQ/default.jpg",
+          "width":120,
+          "height":90
+        },
+        "matches" : [
+          { "start" : "4223.67", "dur" : "4.23", "text" : "engine but it's also possible that" },
+          { "start" : "6934.84", "dur" : "5.73", "text" : "impossible that'll don't ever be able to" }
+        ]
+      },
+      "2" : {
+        "id" : "m70C6N1Y8Ko",
+        "title" : "Joe diGenova Responds to Second Whistleblower",
+        "thumbnail" : {
+          "url" : "https://i.ytimg.com/vi/m70C6N1Y8Ko/default.jpg",
+          "width" : 120,
+          "height" : 90
+        },
+        "matches" : [
+          {"start":"391.039","dur":"3.75","text":"going as long as possible do you think"}
+        ]
+      }
+    },
+  "token":"CAoQAA"
+};
+
+export default data;
